ecd9c4235dab7b0c2cf2d9cc9d6c410652274abd
md <mddasgudiya@gmail.com>
2022-07-24 20:56:57 +0545

    all_category show

---
+++ resources/views/website/all_category.blade.php
@@ -0,0 +1,100 @@
+@extends('layouts.master')
+    @section("content")
+	   @include("website.navbar")
+        <div class="theme-inner-banner">
+				<div class="overlay">
+					<div class="container">
+						<h2>JOB CATEGORY</h2>
+					</div> <!-- /.container -->
+				</div> <!-- /.overlay -->
+			</div> <!-- /.theme-inner-banner -->
+
+			<div class="service-style-one section-spacing">
+				<div class="container">
+					<p class="mb-20" style="text-align: justify; font-size: 16px;">
+					Here is a list of job categories with job posts in Top Job. The list shows the latest online job vacancy by categories. Click on the view job to view list of latest jobs posted by Top Job.
+					</p>
+					<div class="row">
+						<div class="col-xl-9 col-lg-8 col-md-8 col-sm-7 col-12">
+							<div class="row">
+								<div class="col-xl-6 col-lg-6 col-12">
+									<div class="single-service">
+										<div class="img-box"><img src="images/home/3.jpg" alt=""></div>
+										<div class="text">
+											<h5><a href="job-list.html">Constructions</a></h5>
+											<p>If the job requirement</p>
+											<a href="job-list.html" class="read-more">View Job<i class="fa fa-angle-right" aria-hidden="true"></i></a>
+										</div> <!-- /.text -->
+									</div> <!-- /.single-service -->
+								</div> <!-- /.col- -->
+								<div class="col-xl-6 col-lg-6 col-12">
+									<div class="single-service">
+										<div class="img-box"><img src="images/home/4.jpg" alt=""></div>
+										<div class="text">
+											<h5><a href="job-list.html">Security Company</a></h5>
+											<p>If the job requirement</p>
+											<a href="job-list.html" class="read-more">View Job<i class="fa fa-angle-right" aria-hidden="true"></i></a>
+										</div> <!-- /.text -->
+									</div> <!-- /.single-service -->
+								</div> <!-- /.col- -->
+								<div class="col-xl-6 col-lg-6 col-12">
+									<div class="single-service">
+										<div class="img-box"><img src="images/home/5.jpg" alt=""></div>
+										<div class="text">
+											<h5><a href="job-list.html">Supermarket</a></h5>
+											<p>If the job requirement</p>
+											<a href="job-list.html" class="read-more">View Job<i class="fa fa-angle-right" aria-hidden="true"></i></a>
+										</div> <!-- /.text -->
+									</div> <!-- /.single-service -->
+								</div> <!-- /.col- -->
+								<div class="col-xl-6 col-lg-6 col-12">
+									<div class="single-service">
+										<div class="img-box"><img src="images/home/6.jpg" alt=""></div>
+										<div class="text">
+											<h5><a href="job-list.html">Oil & Gas</a></h5>
+											<p>If the job requirement</p>
+											<a href="job-list.html" class="read-more">View Job<i class="fa fa-angle-right" aria-hidden="true"></i></a>
+										</div> <!-- /.text -->
+									</div> <!-- /.single-service -->
+								</div> <!-- /.col- -->
+								<div class="col-xl-6 col-lg-6 col-12">
+									<div class="single-service">
+										<div class="img-box"><img src="images/home/7.jpg" alt=""></div>
+										<div class="text">
+											<h5><a href="job-list.html">Distributors/Suppliers</a></h5>
+											<p>If the job requirement</p>
+											<a href="job-list.html" class="read-more">View Job<i class="fa fa-angle-right" aria-hidden="true"></i></a>
+										</div> <!-- /.text -->
+									</div> <!-- /.single-service -->
+								</div> <!-- /.col- -->
+								<div class="col-xl-6 col-lg-6 col-12">
+									<div class="single-service">
+										<div class="img-box"><img src="images/home/8.jpg" alt=""></div>
+										<div class="text">
+											<h5><a href="job-list.html">Hotels/Resorts/Restaurant</a></h5>
+											<p>If the job requirement</p>
+											<a href="job-list.html" class="read-more">View Job<i class="fa fa-angle-right" aria-hidden="true"></i></a>
+										</div> <!-- /.text -->
+									</div> <!-- /.single-service -->
+								</div> <!-- /.col- -->
+							</div> <!-- /.row -->
+						</div>
+						<div class="col-xl-3 col-lg-4 col-md-4 col-sm-5 col-12 blog-sidebar">
+							<div class="sidebar-container sidebar-categories">
+								<ul>
+									<li><a href="why-us.html">Why Us?</a></li>
+									<li><a href="inner.html">Abou Top Job</a></li>
+									<li><a href="inner.html">Message From Chairman</a></li>
+									<li><a href="requirement-process.html">Recruitment Process</a></li>
+								</ul>
+							</div> <!-- /.sidebar-categories -->
+							<div class="company-slogon">
+								<p class="text-white">we practice Ethical Recruitment and comply to various Business Code of Conducts.</p>
+							</div>
+						</div>
+					</div>
+				</div> <!-- /.container -->
+			</div> <!-- /.service-style-one -->
+          @include("website.company_partner")
+    @endsection
+    
