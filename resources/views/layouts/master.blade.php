bdd7054da71930b07cb61608ee8eebf2361459ed
md <mddasgudiya@gmail.com>
2022-07-25 11:20:01 +0545

    seo finished

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,7 +1,14 @@
 @php
     $global_setting = app\Models\GlobalSetting::all()->first();
 	$normal_gallary_notice = app\Models\Navigation::query()->where('nav_category','Main')->where('page_type','!=','Job')->where('page_type','!=','Photo Gallery')->where('page_type','!=','Notice')->where('parent_page_id',0)->where('page_status','1')->orderBy('position','ASC')->get();
-@endphp
+	if(isset($normal)){
+        $seo = $normal;
+    }
+    elseif(isset($job)){
+        $seo = $job;
+    }
+	@endphp
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -17,7 +24,34 @@
 		<meta name="msapplication-navbutton-color" content="#061948">
 		<!-- iOS Safari -->
 		<meta name="apple-mobile-web-app-status-bar-style" content="#061948">
-		<title>{{$global_setting->page_title ?? 'Raki International | WelCome and Namaste'}}</title>
+		        <!-----SEO--------->
+
+  <title>{{$seo->page_titile ?? $global_setting->page_title}}</title>
+  <meta name="title" content="{{$seo->page_titile ?? $global_setting->page_title}}">
+  <meta name="description" content="{{$seo->page_description ?? $global_setting->page_description}}">
+  <meta name="keywords" content="{{$seo->page_keyword ?? $global_setting->page_keyword}}">
+  <meta name="robots" content="index, follow">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="language" content="English">
+  <meta name="revisit-after" content="1 days">
+  <meta name="author" content="{{$global_setting->site_name ?? ''}}">
+
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="{{$global_setting->website_full_address ?? ''}}">
+  <meta property="og:title" content="{{$seo->page_title ?? $global_setting->page_title}}">
+  <meta property="og:description" content="{{$seo->page_description ?? $global_setting->page_description}}">
+  <meta property="og:image" content="{{$seo->banner_image ?? '/uploads/icons/'.$global_setting->site_logo}}">
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image">
+  <meta property="twitter:url" content="{{$global_setting->website_full_address ?? ''}}">
+  <meta property="twitter:title" content="{{$seo->page_title ?? $global_setting->page_title}}">
+  <meta property="twitter:description" content="{{$seo->page_description ?? $global_setting->page_description}}">
+  <meta property="twitter:image" content="{{$seo->banner_image ?? '/uploads/icons/'.$global_setting->site_logo}}">
+
+<!-----END SEO------->
 		<!-- Favicon -->
 		<link rel="icon" type="image/png" sizes="56x56" href="{{$global_setting->favicon}}">
 		<!-- Main style sheet -->
